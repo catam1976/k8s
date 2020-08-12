70144f1c4000a88d15389a7470ba0067617b436f
Catalin Mihalache <catam1976@gmail.com>
2020-08-12 17:10:03 +0300

    Config Map for PostgreSQL user & password.

--- postgres/undeploy.sh
+++ postgres/undeploy.sh
@@ -5,3 +5,4 @@ kubectl delete deployment postgres-9.6-deployment
 kubectl delete pvc postgres-9.6-pvc
 kubectl delete pv postgres-9.6-pv
 kubectl delete sc postgres-9.6-storage-class
+kubectl delete cm postgres-9.6-config-map
