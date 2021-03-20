e19b1f1f343d1008f44570b48d713757478619ca
Catalin Mihalache <catam1976@gmail.com>
2021-03-20 15:13:58 +0200

    Undeploy secrets and service.

--- postgres/undeploy.sh
+++ postgres/undeploy.sh
@@ -6,3 +6,5 @@ kubectl delete pvc postgres-9.6-pvc
 kubectl delete pv postgres-9.6-pv
 kubectl delete sc postgres-9.6-storage-class
 kubectl delete cm postgres-9.6-config-map
+kubectl delete service postgres-9-6-service
+kubectl delete secret postgres-9.6-secrets
